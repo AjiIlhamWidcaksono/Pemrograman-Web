a0be3c683c15a1da28a394e25e55fd990e19bc6e
AjiIlhamWidcaksono <auliabunganastiti@gmail.com>
2025-04-21 19:12:14 +0700

    Add Commit

---
+++ Tugas 4/Tugas 2.js
@@ -0,0 +1,7 @@
+let cheese = "cheddar";
+
+if (cheese === "cheddar") {
+  console.log("Yay! Cheese available for making cheese on toast.");
+} else {
+  console.log("No cheese on toast for you today.");
+}
